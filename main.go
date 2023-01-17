562f0ac3362fe2a73908fdea1bd12d7eda087401
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2023-01-18 00:10:45 +0700

    [NAMA][NIM][PRODI]

--- main.go
+++ main.go
@@ -6,4 +6,5 @@ func main() {
 	fmt.Println("Hello World!")
 	fmt.Println("Hello World from another device!")
 	fmt.Println("Hello World from another device2!")
+	fmt.Println("Hello World from [NAMA][NIM][PRODI]")
 }
